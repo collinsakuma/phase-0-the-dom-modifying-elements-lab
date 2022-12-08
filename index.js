773c5dfa07fa1da8f9d0f0e8fd1babc6fb7460e5
Collin Sakuma <collinmsakuma@gmail.com>
2022-12-08 11:39:51 -0800

    commit tree c5261ee5cabb6358746a95af62ccc1691b80fa95

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "Collin is the champion";
+document.body.append(newHeader);
+
+//const addId = document.querySelector('h1');
+
+//addId.setAttribute('id','victory');
+
